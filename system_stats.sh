d23aa3a7969f1996367d5a6b39276396af4ec199
theholygun2 <davidstefanusst@gmail.com>
2021-12-13 16:34:35 +0700

    dynamic database

--- system_stats.sh
+++ system_stats.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+user="dbadmin"
+password="inidbadmin"
+database="monitoring"
+
 #---cpu
-cpuUsage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
+cpuUsage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
+#cpuUsage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
+
 #---mem
 mem_used=`free -m | grep "Mem" | awk '{print $3}'`
 
-echo "`date` CPU usage: $cpuUsage | RAM usage: $mem_used MB"
+waktu=`date +'%F %T'`
+cpuUsage=`echo "$cpuUsage" | tr ',' '.'`
 
+mysql --user="$user" --password="$password" --database="$database" --execute="INSERT INTO status VALUES(default,'$waktu',$cpuUsage,$mem_used);"
 
 #---disk
-disk_usage () {
-disk_use=`df -P | grep /dev | grep -v -E '(tmp|boot)' | awk '{print $5}' | cut -f 1 -d "%"`
- echo "disk usage : $disk_use"
-
-}
+# disk_usage () {
+# disk_use=`df -P | grep /dev | grep -v -E '(tmp|boot)' | awk '{print $5}' | cut -f 1 -d "%"`
+#  echo "disk usage : $disk_use"
+# }

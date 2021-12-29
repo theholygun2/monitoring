fe29ed3ab1e2e2be938337766ea78fb8f7ff496b
theholygun2 <davidstefanusst@gmail.com>
2021-12-30 02:41:13 +0700

    update system sh

--- system_stats.sh
+++ system_stats.sh
@@ -14,8 +14,14 @@ mem_used=`free -m | grep "Mem" | awk '{print $3}'`
 waktu=`date +'%F %T'`
 cpuUsage=`echo "$cpuUsage" | tr ',' '.'`
 
+upload=$(speedtest | awk 'BEGIN{ FS=" +|\\(" }; /Upload/{ upl=$2 }; END{ print upl }')
+download=$(speedtest | awk 'BEGIN{ FS=" +|\\(" }; /Download/{ dow=$2 }; END{ print dow }')
+
+
 #mysql --user="$user" --password="$password" --database="$database" --execute="INSERT INTO status VALUES(default,'$waktu',$cpuUsage,$mem_used);"
-mysql monitoring -se "INSERT INTO status VALUES(default,'$waktu',$cpuUsage,$mem_used);"
+mysql monitoring -se "INSERT INTO status VALUES(default,'$waktu',$cpuUsage,$mem_used,$upload,$download);"
+#echo "$waktu,$cpuUsage,$mem_used,$upload,$download"
+#echo "$waktu,$cpuUsage,$mem_used,$upload,$download" >> ~/monitoring/log.csv
 
 #---disk
 # disk_usage () {

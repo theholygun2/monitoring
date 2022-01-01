a857975959949cc67ce592723f168331ac524f11
theholygun2 <davidstefanusst@gmail.com>
2022-01-01 09:20:40 +0700

    merged system

--- system_stats.sh
+++ system_stats.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-user="dbadmin"
-password="inidbadmin"
-database="monitoring"
-
-#---cpu
+#---Menbambil Data CPU usage
 cpuUsage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
-#cpuUsage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
-
-#---mem
-mem_used=`free -m | grep "Mem" | awk '{print $3}'`
-
-waktu=`date +'%F %T'`
+#-- menukar koma dengan titik supaya dapat dilihat decimal
 cpuUsage=`echo "$cpuUsage" | tr ',' '.'`
 
+#cpuUsage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
+
+#---Mengambil Data Memory Ram usage
+mem_used=`free -m | grep "Mem" | awk '{print $3}'`
+
+#--Mengambil current date
+waktu=`date +'%F %T'`
+
+#-- mengambil traffic upload dan download data
+upload=$(speedtest | awk 'BEGIN{ FS=" +|\\(" }; /Upload/{ upl=$2 }; END{ print upl }')
+download=$(speedtest | awk 'BEGIN{ FS=" +|\\(" }; /Download/{ dow=$2 }; END{ print dow }')
+
+# Menggunakan Mysql untuk memasukan variable waktu, cpuUsage, mem_used, upload, download ke dalam tabel Monitoring
+mysql monitoring -se "INSERT INTO status VALUES(default,'$waktu',$cpuUsage,$mem_used,$upload,$download);"
+
 #mysql --user="$user" --password="$password" --database="$database" --execute="INSERT INTO status VALUES(default,'$waktu',$cpuUsage,$mem_used);"
-mysql monitoring -se "INSERT INTO status VALUES(default,'$waktu',$cpuUsage,$mem_used);"
+
+
+
+
+
+
+
+
 
 #---disk
 # disk_usage () {
